55855ce125568b4c78b22b10a48b00a56f3241f8
kris <kris@krisdemacbook-pro.local>
2016-04-15 21:53:21 +0800

    我很嫩嫩嫩嗯嗯呢

--- dfds/dfds/ViewController.m
+++ dfds/dfds/ViewController.m
@@ -17,6 +17,8 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    NSLog(@"我我我哦我我我我我哦我我我");
 }
 
 - (void)didReceiveMemoryWarning {
