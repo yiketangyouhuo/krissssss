b8f3ff07bd608d4f3e3959851def2f40197ab81f
kris <kris@krisdemacbook-pro.local>
2016-04-15 21:30:57 +0800

    我沃尔

--- sdfds/sdfds/ViewController.m
+++ sdfds/sdfds/ViewController.m
@@ -17,6 +17,9 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    NSLog(@"和护手霜");
+    NSLog(@"woowoow");
 }
 
 - (void)didReceiveMemoryWarning {
