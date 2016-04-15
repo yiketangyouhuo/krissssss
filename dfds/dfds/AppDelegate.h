aa24db6dce805f03507845162873873811d6a204
kris <kris@krisdemacbook-pro.local>
2016-04-15 21:40:21 +0800

    我玩我

---
+++ dfds/dfds/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  dfds
+//
+//  Created by kris on 16/4/15.
+//  Copyright © 2016年 kris. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
